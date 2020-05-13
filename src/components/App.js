b0772dd53f646b92b1ed4e77f2a64ba0c2fa21d4
tanvir <mdtanvirislams@gmail.com>
2020-05-13 16:42:09 +0600

    input and search an artist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
+import Artist from './Artist';
+
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
 class App extends Component {
+    state = {artistQuery: '', artist: null, tracks: [] };
+
+    updateArtistQuery = event => {
+        //console.log('event.target.value', event.target.value);
+        this.setState({ artistQuery: event.target.value });
+    }
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();        }
+    }
+
+    searchArtist = () => {
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+                console.log('json', json);
+                if(json.artists.total > 0) {
+                    const artist = json.artists.items[0];
+
+                    //console.log('artist', artist);
+                    
+                    this.setState({ artist });
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(json => this.setState({ tracks: json.tracks }) )
+                        .catch(error => alert(error.message));
+                }
+            })
+            .catch(error => alert(error.message));
+    }
     
     render() {
+        console.log('this.state', this.state);
+        
         return (
-            <div>React App</div>
+            <div>
+                <h2>Music Artist Finder</h2>
+                <input 
+                    onChange={this.updateArtistQuery} 
+                    onKeyPress={this.handleKeyPress}
+                    placeholder='Search for an artist'
+                />
+                <button onClick={this.searchArtist}>Search</button>
+                <Artist artist={this.state.artist}/>
+            </div>
         )
     }
 }
